febad99e95dd83d8a282a56a9794d6c25dcb2d3e
CS student <me@DebianXfce23F.cs.uoregon.edu>
2026-04-16 13:50:21 -0700

    made server work

--- app.py
+++ app.py
@@ -7,16 +7,14 @@ app = Flask(__name__)
 
 mongo_host = os.environ.get('DB_HOST', 'db')
 client = MongoClient(mongo_host, 27017)
-db = client.timesdb
+db = client.classesdb
 
 @app.route("/")
 @app.route("/index")
 def home():
-    _items = db.timesdb.find()
-    items = [item for item in _items]
     app.logger.debug("Main page entry")
 
-    return flask.render_template('calc.html')
+    return flask.render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
